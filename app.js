7ad712c97183a449b30f525dd7bad0101befd1bc
mikealessi24 <60523720+mikealessi24@users.noreply.github.com>
2021-01-02 17:40:55 -0500

    created get and post item routes

--- app.js
+++ app.js
@@ -15,4 +15,35 @@ const pool = sql.createPool({
   password: process.env.password,
 });
 
+app.post("/list-item", async (req, resp) => {
+  try {
+    const name = req.body.name;
+    const description = req.body.description;
+    const price = req.body.price;
+    const image = req.body.image;
+    const quantity = req.body.quantity;
+
+    const response = await conn.execute(
+      "INSERT INTO eGrocery.items (name, description, price, image, qunatity) VALUES (?,?,?,?,?)",
+      [name, description, price, image, quantity]
+    );
+    resp.status(200).send({ message: "item successfully listed" });
+  } catch (error) {
+    resp.status(500).send(error);
+    console.log(error);
+  }
+});
+
+app.post("/get-items", async (req, resp) => {
+  try {
+    const conn = await pool.getConnection();
+    const response = await conn.execute("SELECT * FROM eGrocery.items");
+    conn.release();
+    resp.status(200).send(response[0]);
+  } catch (error) {
+    resp.status(500).send(error);
+    console.log(error);
+  }
+});
+
 app.listen(PORT, () => console.log("app is listening on", PORT));
